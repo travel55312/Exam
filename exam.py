30d513d543ee5ee0fed9eb69ebc15c157a57b154
Liang <travel55312@gmail.com>
2024-08-17 10:38:22 +0800

    邏輯測驗fin

--- exam.py
+++ exam.py
@@ -1,7 +1,7 @@
 def Cal_Fin_Amnt(Org_Amnt, periods, periods_rt):
     Fin_Amnt = round(Org_Amnt * ((1 + periods_rt) ** periods) ,0)
-    Fin_Amnt = "{:,.0celf}".format(Fin_Amnt)
+    Fin_Amnt = "{:,.0f}".format(Fin_Amnt)
     return Fin_Amnt
 
 TestResult = Cal_Fin_Amnt(500000, 24, 0.06)
-print(f"期滿後累積的金額為：{TestResult} 元")
+print(f"期滿後累積的金額為: {TestResult} 元")
